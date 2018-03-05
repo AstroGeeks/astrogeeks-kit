64c8f1e94aca0bc0ce67e3ec21e6e9c53dd8d012
AstroGeeks <astrogeeksgent@outlook.be>
2018-03-05 19:49:02 +0100

    Wrap everything in functions.

--- astroplant_kit/core.py
+++ astroplant_kit/core.py
@@ -14,37 +14,49 @@ from .kit import Kit
 from . import config
 
 def main():
-    # Logging
+    init_logger()
+    conf = read_config()
+    init_client(conf)
+    run_kit()
+
+def init_logger():
     ## create logger
     logger = logging.getLogger("AstroPlant")
     logger.setLevel(logging.DEBUG)
 
-    ## create console handler and set level to debug
+    ## create console handler
     ch = logging.StreamHandler()
     ch.setLevel(logging.DEBUG)
 
     ## create formatter
     formatter = logging.Formatter('%(asctime)s - %(threadName)s - %(name)s - %(levelname)s - %(message)s')
 
-    ## add formatter to ch
+    ## add formatter to console handler
     ch.setFormatter(formatter)
 
-    ## add ch to logger
+    ## add console handler to logger
     logger.addHandler(ch)
 
+def read_config():
+    logger = logging.getLogger("AstroPlant")
     logger.info('Reading configuration.')
     try:
         conf = config.read_config()
     except Exception as e:
         logger.error('Exception while reading configuration: %s' % e)
         sys.exit(e.errno)
-    
+    return conf
+
+def init_client(conf):
+    logger = logging.getLogger("AstroPlant")
     logger.info('Creating AstroPlant network client.')
     api_client = Client(conf["api"]["root"], conf["websockets"]["url"])
 
     logger.info('Authenticating AstroPlant network client.')
     api_client.authenticate(conf["auth"]["serial"], conf["auth"]["secret"])
-    
+
+def run_kit():
+    logger = logging.getLogger("AstroPlant")
     logger.info('Initialising kit.')
     kit = Kit(api_client, conf["debug"])
     kit.run()
